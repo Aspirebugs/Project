dbbffb6d135a0b7289b22ee8ca081fc85beceeb0
Aspirebugs <2022kucp1109@iiitkota.ac.in>
2025-08-17 19:10:42 +0530

    changed routing configuration

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,15 @@ dotenv.config();
 const app = express();
 
 const allowed = [
-  "http://localhost:5173",
-  "http://localhost:3000",
-  /\.vercel\.app$/
+  "https://project-mu-three-59.vercel.app" ,
+  "https://project-krishs-projects-a41f1900.vercel.app",
+  "https://project-git-master-krishs-projects-a41f1900.vercel.app"
 ];
 
 app.use(cors({
   origin(origin, cb) {
     if (!origin) return cb(null, true);
-    if (allowed.some((a) => a instanceof RegExp ? a.test(origin) : a === origin)) return cb(null, true);
+    if (allowed.includes(origin)) return cb(null, true);
     cb(new Error("Not allowed by CORS"));
   },
   credentials: true
